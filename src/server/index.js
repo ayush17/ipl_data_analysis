6002af3e1e62c98db572d3c87f856e435a407dbb
ayush17 <ayushtiwari17011995@gmail.com>
2019-11-25 09:07:58 +0530

    review test

--- src/server/index.js
+++ src/server/index.js
@@ -27,8 +27,25 @@ var converting_bowlers_to_string=JSON.stringify(bowlers)//converting objects to 
 
 fs.writeFileSync("../output/Top_Ten_Economic_Bowlers.json",converting_bowlers_to_string,'utf-8')
 //review------------------------------
+ //mostValuablePalyer
 const frr=require('./review.js');
 var Players=frr.mostValuablePlayer(matches)
 var converting_Players_to_string=JSON.stringify(Players)//converting objects to string
 
 fs.writeFileSync("../output/Valuable_Player.json",converting_Players_to_string,'utf-8')
+ //number of teams who won the match and also won the toss
+ var teams=frr.teamsWhoWonTheTossAndMatch(matches)
+ var converting_teams_to_string=JSON.stringify(teams)
+ fs.writeFileSync("../output/Number_Of_Times_Winning_Toss_Team_Won_The_Match.json", converting_teams_to_string,'utf-8')
+ //strike rate of Virat kholi for each season
+ var strikeRateVirat=frr.strikeRateOfVirat(matches,deliveries)
+ var converting_strikeRateVirat_to_string=JSON.stringify(strikeRateVirat)
+ fs.writeFileSync("../output/Strike_Rate_Of_Virat_Kholi_Every_Season.json", converting_strikeRateVirat_to_string,'utf-8')
+ var bowler=frr.bowlerWithBestEconomy(deliveries)
+ var converting_bowler_to_string=JSON.stringify(bowler)
+ fs.writeFileSync("../output/Bowler_Which_Has_Best_Economy_In_Super_Over.json", converting_bowler_to_string,'utf-8')
+ 
+ var dismissed=frr. highestNumberOfTimesOnePlayerDismissedOther(deliveries)
+ var converting_dismissed_to_string=JSON.stringify(dismissed)
+ fs.writeFileSync("../output/Number_Of_Times_One_Player_dismissed_Other_Player.json", converting_dismissed_to_string,'utf-8')
+ 
--- src/server/review.js
+++ src/server/review.js
@@ -11,7 +11,7 @@ function mostValuablePlayer(matches) {
   }, {});
 
   let arrayOfEachSeason = Object.entries(player);
-  console.log(arrayOfEachSeason);
+  
 
   let temp = arrayOfEachSeason.reduce((eachSeason, eachSeasonEntires) => {
     eachSeason[[eachSeasonEntires[0]]] = Object.entries(eachSeasonEntires[1]);
@@ -35,7 +35,113 @@ function mostValuablePlayer(matches) {
 
   return mostValuablePlayerPerSeason;
 }
+function teamsWhoWonTheTossAndMatch(matches){
+  var arrayOfEveryObjectsValues=matches.reduce((object,objectvalues)=>{ //pushing every object values inside matches as array
+    object.push(Object.values(objectvalues)) 
+    return object
+  },[]);
+      arrayOfEveryObjectsValues
+      var filteredArrayOfEveryObjectsValues=arrayOfEveryObjectsValues.filter(first=>first[6]==first[10])//filtering only those matches in which toss and winner team are same 
+      var countOfTeamsWhoWonTheMatchAndToss=filteredArrayOfEveryObjectsValues.reduce((acc,array)=>{
+        if(acc[array[6]])
+         acc[array[6]]++;
+        else
+        acc[array[6]]=1;
+        return acc;
+      },{});
+      return countOfTeamsWhoWonTheMatchAndToss 
+  }
 
+function strikeRateOfVirat(matches,deliveries,){
+  
+  var runsAndBallsPerMatch=deliveries.reduce((acc,data)=>{
+    if(data["batsman"]=="V Kohli"){
+     if(acc[data["match_id"]]){
+       if(acc[data["match_id"]]["runs"]){
+        acc[data["match_id"]]["runs"]+=parseInt(data["batsman_runs"])
+        acc[data["match_id"]]["balls"]++;
+       }
+        
+     }
+     else   {
+     acc[data["match_id"]] = {}
+      acc[data["match_id"]]["runs"]=parseInt(data["batsman_runs"])
+      acc[data["match_id"]]["balls"]=1
+      
+     }
+    }    
+  return acc
+  },{})
+  let strikeRatePerSeason={}
+  let totalruns=0;
+  let totalballs=0;
+  for(let matchid in runsAndBallsPerMatch){
+    for(let id in matches ){
+      if(matchid==matches[id]["id"]){
+        
+        //insert season in new object
+        if(strikeRatePerSeason[matches[id]["season"]]){
+          //add every matches runs and every matches balls
+           totalruns+=runsAndBallsPerMatch[matchid]["runs"];
+           totalballs+=runsAndBallsPerMatch[matchid]["balls"];
+           //divide runs by balls and multiply by 100
+          //save the result as new strike rate for that season
+           strikeRatePerSeason[matches[id]["season"]]["strike_rate"]=((totalruns/totalballs)*100).toFixed(2);
+        }
+        else{
+          strikeRatePerSeason[matches[id]["season"]]={}
+           totalruns=0;
+           totalballs=0;
+        }
+        
+        
+      }
+    }
+  }
+
+  return strikeRatePerSeason
+}
+function bowlerWithBestEconomy(deliveries){
+  let onlySuperOvers=deliveries.filter(match=>match["is_super_over"]!="0")
+  
+let collectionOfBowlersWhoBowledInSuperOverWithRuns=onlySuperOvers.reduce((bowlers,matches)=>{
+      if(bowlers[matches["bowler"]])
+        bowlers[matches["bowler"]] +=parseInt(matches["total_runs"])
+      else
+      bowlers[matches["bowler"]] =parseInt(matches["total_runs"])
+  return bowlers
+},{});
+var arrayOfBestBowlersRuns=Object.values(collectionOfBowlersWhoBowledInSuperOverWithRuns).sort((a,b)=>a-b).slice(0,1)
+var theBestBowlerWhoGaveLeastRuns=[]
+for(let key in collectionOfBowlersWhoBowledInSuperOverWithRuns){
+  if(collectionOfBowlersWhoBowledInSuperOverWithRuns[key]==arrayOfBestBowlersRuns[0]){
+    theBestBowlerWhoGaveLeastRuns.push(key)
+  }
+}
+return theBestBowlerWhoGaveLeastRuns
+}
+function highestNumberOfTimesOnePlayerDismissedOther(deliveries){
+  let onlyDismissed=deliveries.filter(match=>match["player_dismissed"]!="")
+   let numberOftimesOnePlayerIsDismissedByOther=onlyDismissed.reduce((dismissed,matches)=>{
+          if(dismissed[matches['player_dismissed']]){
+             if(dismissed[matches['player_dismissed']][matches["bowler"]])
+             dismissed[matches['player_dismissed']][matches["bowler"]]++;
+             else
+             dismissed[matches['player_dismissed']][matches["bowler"]]=1;
+            
+          }
+          else{
+            dismissed[matches['player_dismissed']]={}
+            dismissed[matches['player_dismissed']][matches["bowler"]]=1;
+          }
+    return dismissed
+   },{});
+   return numberOftimesOnePlayerIsDismissedByOther
+}
 module.exports = {
-  mostValuablePlayer
+  mostValuablePlayer,
+  teamsWhoWonTheTossAndMatch,
+  strikeRateOfVirat,
+  bowlerWithBestEconomy,
+  highestNumberOfTimesOnePlayerDismissedOther
 };
